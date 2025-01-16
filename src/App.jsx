066195cb84bfdd16579c0721e593df8682570ca7
thounny <thounnykeo@gmail.com>
2025-01-15 19:53:07 -0500

    Imported Story section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import NavBar from './components/NavBar'
 import Features from './components/Features'
+import Story from './components/Story'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <About />
       <Features />
+      <Story />
     </main>
   )
 }
