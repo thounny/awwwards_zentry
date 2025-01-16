84ac3fefc80c378cf1d51a03e7ec71870016ab9f
thounny <thounnykeo@gmail.com>
2025-01-15 21:18:36 -0500

    Updated color and fonts

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,23 @@
 /* eslint-disable react/prop-types */
 import clsx from "clsx";
 
-const Button = ({ id, title, rightIcon, leftIcon, containerClass }) => {
+const Button = ({ id, title, rightIcon, leftIcon, containerClass, url }) => {
+    const Element = url ? "a" : "button"; // Use <a> if url is provided, otherwise <button>
+
     return (
-        <button
+        <Element
             id={id}
+            href={url} // Only applies if it's an <a> tag
+            target={url ? "_blank" : undefined} // Open link in a new tab
+            rel={url ? "noopener noreferrer" : undefined} // Security for external links
             className={clsx(
-                "group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black",
+                "group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-pink-300 px-7 py-3 text-black",
                 containerClass
             )}
         >
             {leftIcon}
 
-            <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+            <span className="relative inline-flex overflow-hidden font-helvetica-neue text-xs uppercase">
                 <div className="translate-y-0 skew-y-0 transition duration-500 group-hover:translate-y-[-160%] group-hover:skew-y-12">
                     {title}
                 </div>
@@ -22,7 +27,7 @@ const Button = ({ id, title, rightIcon, leftIcon, containerClass }) => {
             </span>
 
             {rightIcon}
-        </button>
+        </Element>
     );
 };
 
