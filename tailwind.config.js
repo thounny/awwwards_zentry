066ef744815f7c371b93f8fcf3964af233695a54
thounny <thounnykeo@gmail.com>
2025-01-15 21:19:53 -0500

    Imported Helvetica Neue (goated)

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
         "circular-web": ["circular-web", "sans-serif"],
         "robert-medium": ["robert-medium", "sans-serif"],
         "robert-regular": ["robert-regular", "sans-serif"],
+        "helvetica-neue": ["helvetica-neue", "helvetica", "sans-serif"],
       },
       colors: {
         blue: {
