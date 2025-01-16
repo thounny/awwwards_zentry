ad4edc4747f0c1e7d23bbc8cd9544dabe36c7900
thounny <thounnykeo@gmail.com>
2025-01-15 21:18:21 -0500

    Updated color and fonts

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
     return (
         <div id="about" className="min-h-screen w-screen">
             <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-                <p className="font-general text-sm uppercase md:text-[10px]">
+                <p className="font-helvetica-neue text-sm uppercase md:text-[10px]">
                     Welcome to Tone&apos;s World
                 </p>
 
